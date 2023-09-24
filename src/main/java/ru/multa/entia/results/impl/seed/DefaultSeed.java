b0e7c7cfdfca0484c3684a385a44f9bfa042d0ad
KasymbekovPN <kasymbekovPN@yandex.ru>
2023-09-24 16:44:56 +0300

    DefaultSeedFactory is added

--- src/main/java/ru/multa/entia/results/impl/seed/DefaultSeed.java
+++ src/main/java/ru/multa/entia/results/impl/seed/DefaultSeed.java
@@ -2,7 +2,4 @@ package ru.multa.entia.results.impl.seed;
 
 import ru.multa.entia.results.api.seed.Seed;
 
-record DefaultSeed(String code, Object... args) implements Seed {
-    public static final String DEFAULT_CODE = "";
-    public static final Object[] DEFAULT_ARGS = new Object[0];
-}
+record DefaultSeed(String code, Object... args) implements Seed {}
